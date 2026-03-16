71995d5d22b9cc141ec8528e5c42aa27e7a2e847
Intensegamer3 <ol.erlandsson@gmail.com>
2026-03-16 12:48:19 +0100

    Ported over stuff I worked on from home

--- lab-1-game-feel/Scripts/PlayerNode.cs
+++ lab-1-game-feel/Scripts/PlayerNode.cs
@@ -3,45 +3,93 @@ using System;
 public partial class PlayerNode : CharacterBody3D
 {
 
-	int movementStyle = 0;
+	int movementMode = 1;
 
-	Vector3 movementDirection = Vector3.Zero;
-	float maxSpeed = 5f;
-	float acceleration = 0f;
-	float deceleration = 0f;
-
-	Vector3 velocity;
+	public float movementOneSpeed = 5f;
+	
+	public float movementTwoAcceleration = 0.02f;
+	public float movementTwoDeceleration = 3;
+	public float movementTwoMaxSpeed = 5;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
-	{
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
 	{
 		
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Velocity = velocity;
-	}
+		Vector2 inputDir = Input.GetVector("MoveWest", "MoveEast", "MoveNorth", "MoveSouth");
 
-	void MoveForward()
-	{
-		velocity = Velocity;
-		switch (movementStyle)
+		Vector3 direction = new Vector3(inputDir.X, 0, inputDir.Y).Normalized();
+
+		if(movementMode == 1) // Immediate acceleration, and immediate stop at deceleration
 		{
-			case 0:
-				if (Input.IsActionPressed("Forward"))
-				{
-					
-				}
-				break;
-				
-				
-
+			if (direction != Vector3.Zero)
+			{
+				Velocity = direction * movementOneSpeed;
+			}
+			else
+			{
+				Velocity = Vector3.Zero;
+			}
 		}
+		else if (movementMode == 2) // Slow constant acceleration, fast constant deceleration
+		{
+			if (direction != Vector3.Zero)
+			{
+				Vector3 targetVelocity = direction * movementTwoMaxSpeed;
+				Vector3 acceleration = targetVelocity * movementTwoAcceleration;
+
+				Velocity += acceleration;
+				Velocity.Clamp(Vector3.Zero, targetVelocity);
+			}
+			else
+			{
+				if(Velocity.Length() > 0.1f)
+				{
+					Vector3 deceleration = -Velocity.Normalized() * movementTwoDeceleration * (float)delta;
+					Velocity = Velocity + deceleration;
+				}
+				else
+				{
+					Velocity = Vector3.Zero;
+				}
+			}
+		}
+		else if (movementMode == 3) // Ease acceleration, and constant deceleration
+		{
+			if (direction != Vector3.Zero)
+			{
+				Velocity = direction * movementOneSpeed;
+			}
+			else
+			{
+				Velocity = Vector3.Zero;
+			}
+		}
+		else
+		{
+			return;
+		}
+		MoveAndSlide();
 	}
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event.IsActionPressed("SwitchMovementModeTo1"))
+		{
+			movementMode = 1;
+		}
+		
+		if (@event.IsActionPressed("SwitchMovementModeTo2"))
+		{
+			movementMode = 2;
+		}
+
+		if (@event.IsActionPressed("SwitchMovementModeTo3"))
+		{
+			movementMode = 3;
+		}
+    }
 }
